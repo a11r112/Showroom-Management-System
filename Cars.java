dd551a1601fff4eda6f3ae1de40e239f67edbb40
Aniket Kumar Ram <87979925+a11r112@users.noreply.github.com>
2024-02-20 13:40:29 +0530

    Update Cars.java

--- Cars.java
+++ Cars.java
@@ -2,37 +2,53 @@ import java.util.Scanner;
 
 
 public class Cars extends Showroom {
-    String Carname;
-    String Cartype;
-    String Carcolor;
-    int Carprice;
+    String Carname[]=new String[5];
+    String Cartype[]= new String[5];
+    String Carcolor[]=new String [5];
+    int Carprice[]=new int[5];
+    int currentIndex=0;
+           Scanner C= new Scanner(System.in);
+
+
+
     public void get_detail() {
 
-        System.out.println("\nCar Name :"+Carname);
-        System.out.println("Car Type :"+Cartype);
-        System.out.println("Car colour : "+Carcolor);
-        System.out.println("Car Price: "+ Carprice);
+        for (int i=0;i<currentIndex ;i++ ) {
+        
+        System.out.println("\nCar Name :"+Carname[i]);
+        System.out.println("Car Type :"+Cartype[i]);
+        System.out.println("Car colour : "+Carcolor[i]);
+        System.out.println("Car Price: "+ Carprice[i]);
         System.out.println("");
+ 	
+        }
 
+        
     }
 
     public void set_detail() {
-    	    	    	    	            	System.out.println("\nPlease Enter the required details----->\n");
+    	System.out.println("\nPlease Enter the required details----->\n");
 
-       Scanner C= new Scanner(System.in);
+        if (currentIndex<5){
         System.out.println("\nCar Name :");
-                Carname = C.nextLine();
+                Carname[currentIndex] = C.nextLine();
 
         System.out.println("\nCar Type \n(Petrol/ disel/ Hybrid/ Electric)");
-                Cartype = C.nextLine();
+                Cartype[currentIndex] = C.nextLine();
 
         System.out.println("\nCar colour : ");
-                Carcolor = C.nextLine();
+                Carcolor[currentIndex] = C.nextLine();
 
         System.out.println("\nCar Price: ");
-                Carprice = C.nextInt();
+                Carprice[currentIndex] = C.nextInt();
                         System.out.println("\nYour Car has been successfully added----->\n");
+                        currentIndex++;
+                        C.nextLine();
 
 
     }
+    else{
+    	System.out.println("You have reched your limit for adding Cars------->");
+    }
+}
 }
