4e8c6b5ceb3b05df9ecd44b11380335483465ac0
Aniket Kumar Ram <87979925+a11r112@users.noreply.github.com>
2024-02-20 13:41:57 +0530

    Update Showroom.java

--- Showroom.java
+++ Showroom.java
@@ -1,47 +1,51 @@
 import java.util.Scanner;
+
 public class Showroom implements M.Utility {
-    String[] Showroom_name = new String[5];
-    String[] Manager_name = new String[5];
-    String[] Address = new String[5];
-    int[] n_employee = new int[5];
-    int[] car_available = new int[5];
-    int index = -1; // Index to keep track of the current slot in the array
+    String[] showroom_names = new String[5];
+    String[] manager_names = new String[5];
+    String[] addresses = new String[5];
+    int[] n_employees = new int[5];
+    int[] cars_available = new int[5];
+    int currentIndex = 0;
+    Scanner scanner = new Scanner(System.in);
 
     public void get_detail() {
-        if (index >= 0 && index < 5) {
-            System.out.println("\nShowroom name: " + Showroom_name[index]);
-            System.out.println("Manager name: " + Manager_name[index]);
-            System.out.println("Address: " + Address[index]);
-            System.out.println("No of Employees: " + n_employee[index]);
-            System.out.println("No of cars available: " + car_available[index]);
+        for (int i = 0; i < currentIndex; i++) {
+            System.out.println("\nShowroom name: " + showroom_names[i]);
+            System.out.println("Manager name: " + manager_names[i]);
+            System.out.println("Address: " + addresses[i]);
+            System.out.println("No of Employees: " + n_employees[i]);
+            System.out.println("No of cars available: " + cars_available[i]);
             System.out.println("\n");
-        } else {
-            System.out.println("No data available for this index.");
         }
     }
 
-    public void set_detail() {
-        index++;
-        if (index < 5) {
-            Scanner Sc = new Scanner(System.in);
+public void set_detail() {
+    System.out.println("\nPlease Enter the required details----->\n");
+    if (currentIndex < 5) {
+        System.out.println("\nEnter Your Showroom name: ");
+        showroom_names[currentIndex] = scanner.nextLine();
 
-            System.out.println("\nEnter Your Showroom name: ");
-            Showroom_name[index] = Sc.nextLine();
+        System.out.println("\nEnter Manager name: ");
+        manager_names[currentIndex] = scanner.nextLine();
 
-            System.out.println("\nEnter Manager name: ");
-            Manager_name[index] = Sc.nextLine();
+        System.out.println("\nEnter Address: ");
+        addresses[currentIndex] = scanner.nextLine();
 
-            System.out.println("\nEnter Address: ");
-            Address[index] = Sc.nextLine();
+        System.out.println("\nNo of Employees: ");
+        n_employees[currentIndex] = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
 
-            System.out.println("\nNo of Employees: ");
-            n_employee[index] = Sc.nextInt();
-            System.out.println("\nNo of cars available: ");
-            car_available[index] = Sc.nextInt();
-            System.out.println("\nYour Showroom Added Successfully---->\n");
-        } else {
-            System.out.println("Maximum number of showrooms reached.");
-            index--; // Decrement index as no new data was added
-        }
+        System.out.println("\nNo of cars available: ");
+        cars_available[currentIndex] = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+
+        currentIndex++;
+        System.out.println("\nYour Showroom Added Successfully---->\n");
+    } else {
+        System.out.println("\nYou have reached the maximum number of showrooms.\n");
     }
 }
+
+    }
+
